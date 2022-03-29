32269ef1057d30a0e129beb7a8f0a9222dff329d
Dorian Naujokat <35139623+doriannaujokat@users.noreply.github.com>
2022-03-29 17:36:11 +0200

    Update spotify.js
    - Implement global instance
    - Implement ApiFetchAll method
    - Update ApiFetch method to be similar to ApiFetchAll

--- spotify.js
+++ spotify.js
@@ -10,7 +10,14 @@ function Spotify(apiToken) {
     this.apiToken = apiToken;
 }
 
+
+Spotify.prototype.updateToken = function updateToken(apiToken) {
+    this.apiToken = apiToken;
+    return this;
+}
+
 Spotify.BaseURL = "https://api.spotify.com/v1";
+Spotify.global = new Spotify(null);
 
 class SpotifyApiError extends Error {
     SPOTIFY_ERROR;
@@ -26,31 +33,70 @@ class SpotifyApiError extends Error {
 
 /**
  * @param {string} endpoint
- * @param {"GET"|"POST"|"PUT"|"DELETE"} method
- * @param {?Object} [body]
+ * @param {?{[method]: "GET"|"POST"|"PUT"|"DELETE", [body]: Object, [rejectOnRateLimit]: boolean, [rejectOnInvalidToken]: boolean, [invalidTokenCallback]: function}} [options]
  * @return {Promise<unknown>}
  * @this {Spotify}
  * @throws {SpotifyApiError}
  */
-async function ApiFetch(endpoint, method, body) {
-    return new Promise((resolve, reject) => {
-        const req = new Request(`${Spotify.BaseURL}${endpoint}`, {method: method, headers: {Authorization: `Bearer ${this.apiToken}`, 'Content-Type': 'applications/json'}});
-        if (body) req.body = JSON.stringify(body);
-        fetch(req).then(async function (resp) {
-            if (resp.status === 401) {
-                reject(new SpotifyApiError('INVALID_TOKEN', 401,'Invalid API Token')); // TODO
-                return;
-            }
-            if (resp.status === 200) {
-                const jresp = await resp.json();
-                resolve(jresp);
-                return;
-            }
-        }).catch(async function (error) {
-            reject(error);
-        });
-    });
+async function ApiFetch(endpoint, options) {
+    const req = new Request(`${Spotify.BaseURL}${endpoint}`, {method: options?.method??"GET", headers: {Authorization: `Bearer ${this.apiToken}`, 'Content-Type': 'application/json'}, body: options?.body ? JSON.stringify(options.body) : undefined});
+    for (let retry = 0; retry < 10; retry++) {
+        const resp = await fetch(req);
+        if (resp.status === 401) {
+            options?.invalidTokenCallback?.();
+            if (options?.rejectOnInvalidToken ?? true) throw new SpotifyApiError('INVALID_TOKEN', 401,'Invalid API Token');
+            // TODO Await token update
+        }
+        if (resp.status === 429) {
+            if (options?.rejectOnRateLimit) throw new SpotifyApiError('RATE_LIMIT', 429,'The rate limit has been reached');
+            await new Promise(resolve => setTimeout(resolve, parseInt(resp.headers.get('Retry-After')??'10')*1000));
+            continue;
+        }
+        if (resp.status === 200) {
+            const jresp = await resp.json();
+            return jresp;
+        }
+        return undefined;
+    }
+    throw new SpotifyApiError('RETRY_LIMIT_EXCEEDED', 0,'Could not get a response within 10 tries');
 }
 Spotify.prototype.ApiFetch = ApiFetch;
+
+/**
+ * @param {string} endpoint
+ * @param {function} [progressCb]
+ * @param {?{[method]: "GET"|"POST"|"PUT"|"DELETE", [limit]: number, [delay]: number, [rejectOnInvalidToken]: boolean, [invalidTokenCallback]: function}} [options]
+ * @return {Promise<any[]>}
+ */
+async function ApiFetchAll(endpoint, progressCb, options){
+    const results = [];
+    let req = new Request(`${Spotify.BaseURL}${endpoint}${options?.limit?`?limit=${options.limit}`:``}`, {method: options?.method??"GET", headers: {Authorization: `Bearer ${this.apiToken}`, 'Content-Type': 'application/json'}});
+    while (true) {
+        const resp = await fetch(req);
+        if (resp.status === 401) {
+            options?.invalidTokenCallback?.();
+            if (options?.rejectOnInvalidToken ?? true) throw new SpotifyApiError('INVALID_TOKEN', 401,'Invalid API Token');
+            // TODO Await token update
+        }
+        if (resp.status === 429) {
+            await new Promise(resolve => setTimeout(resolve, parseInt(resp.headers.get('Retry-After')??'10')*1000));
+            continue;
+        }
+        if (resp.status === 200) {
+            const jresp = await resp.json();
+            for (let item of jresp.items) {
+                results.push(item);
+            }
+            progressCb?.(jresp);
+            if (jresp.next === undefined || jresp.next === null) break;
+            req = new Request(jresp.next, {method: options?.method??"GET", headers: {Authorization: `Bearer ${this.apiToken}`, 'Content-Type': 'application/json'}});
+            if (options.delay) await new Promise(resolve => setTimeout(resolve, options.delay));
+            continue;
+        }
+        throw new SpotifyApiError('INVALID_RESPONSE', 0,'Received an invalid response');
+    }
+    return results;
+}
+Spotify.prototype.ApiFetchAll = ApiFetchAll;
 
 export { Spotify as Spotify };
